54bb804b7d030aa07d895b94923627b63a30bdab
Gresy Kohli <114817484+GresyKohli@users.noreply.github.com>
2024-02-15 19:34:15 +0530

    Arrayss.java

--- Arrayss.java
+++ Arrayss.java
@@ -4,16 +4,14 @@ class Arrayss
 {
 public static void main(String args[])
 {
-int arr[] = {10, 20, 30, 40, 50};
+int arr[] = {10, 50, 20, 80, 100, 30};
 System.out.println("Greatest Elements in given array are : ");
-for(int i = 1; i <= 4; i++)
+for(int i = 1; i <= 5; i++)
 {
 if(arr[0] < arr[i])
 {
 arr[0] = arr[i];
 }
-else
-break;
 }
 System.out.println(+arr[0]);
 }
