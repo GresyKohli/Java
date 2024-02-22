e8a5fad5cbb00b219a2938dd170c9af6d6f066cf
Gresy Kohli <114817484+GresyKohli@users.noreply.github.com>
2024-02-22 18:05:02 +0530

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,63 +1,34 @@
-class A  // Super class
-{
-float a, b, c;
-void add()
-{
-a = 100;
-b = 65;
-c = a + b;
-System.out.println("The addition of two numbers is : " +c);
-}
-}
-
-class B extends A  // Sub class 1
-{
-void sub()
-{
-a = 100;
-b = 65;
-c = a - b;
-System.out.println("The subtraction of two numbers is : " +c);
-}
-
-void mul()
-{
-a = 100;
-b = 65;
-c = a * b;
-System.out.println("The multiplication of two numbers is : " +c);
-}
-
-void remain()
-{
-a = 100;
-b = 65;
-c = a % b;
-System.out.println("The remainder of two numbers is : " +c);
-}
-}
-
-
-class C extends B  // Sub class 2
-{
-void div()
-{
-a = 100;
-b = 65;
-c = a / b;
-System.out.println("The division of two numbers is : " +c);
-}
-}
-
+import java.util.Scanner;
 class Calculator
 {
 public static void main(String args[])
 {
-C obj = new C();
-obj.add();
-obj.sub();
-obj.mul();
-obj.remain();
-obj.div();
+int num1, num2;
+char choices;
+Scanner s = new Scanner(System.in);
+System.out.println("Enter any two integers : ");
+num1 = s.nextInt();
+num2 = s.nextInt();
+System.out.println("Enter any operator among these +, -, *, /, % : ");
+choices = s.next().charAt(0); 
+
+switch(choices)
+{
+case '+':
+	 System.out.println("The addition of the given numbers is : " +(num1 + num2));
+	 break;
+ case '-':
+	 System.out.println("The subtraction of the given numbers is : " +(num1 - num2));
+	 break;
+ case '*':
+	 System.out.println("The multiplication of the given numbers is : " +(num1 * num2));
+	 break;
+ case '/':
+	 System.out.println("The modulus of the given numbers is : " +(num1 % num2));
+	 break;
+ case '%':
+	 System.out.println("The division of the given numbers is : " +(num1 / num2)); 
+	 break; 
+}
 }
 }
